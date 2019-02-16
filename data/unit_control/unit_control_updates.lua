32bed7c7a60f8e0dc8954769170efce3c11b1d4e
Klonan <11986037+Klonan@users.noreply.github.com>
2019-02-16 23:57:54 +0100

    fix 2

--- data/unit_control/unit_control_updates.lua
+++ data/unit_control/unit_control_updates.lua
@@ -4,19 +4,19 @@ local tools = names.unit_tools
 local path = util.path("data/unit_control/")
 
 local deployer_tool = data.raw["selection-tool"][tools.deployer_selection_tool]
-local entity_filter = deployer_tool.entity_filter
-local alt_filter = deployer_tool.alt_entity_filter
+local entity_filter = deployer_tool.entity_filters
+local alt_filter = deployer_tool.alt_entity_filters
 
 for name, entity in pairs (data.raw["assembling-machine"]) do
   if entity.is_deployer then
     table.insert(entity_filter, entity.name)
-    table.insert(alt_entity_filter, entity.name)
+    table.insert(alt_filter, entity.name)
   end
 end
 
 for name, entity in pairs (data.raw["furnace"]) do
   if entity.is_deployer then
     table.insert(entity_filter, entity.name)
-    table.insert(alt_entity_filter, entity.name)
+    table.insert(alt_filter, entity.name)
   end
 end
