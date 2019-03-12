cd13130b7cb1d1c9894c418f6faf8eda9d5f95ba
Klonan <11986037+Klonan@users.noreply.github.com>
2019-03-12 17:05:11 +0100

    version bum,p and localse fix

--- data/hotkeys.lua
+++ data/hotkeys.lua
@@ -4,7 +4,7 @@ local move_unit =
 {
   type = "custom-input",
   name = hotkeys.unit_move,
-  localised_names = hotkeys.unit_move,
+  localised_name = hotkeys.unit_move,
   key_sequence = "SHIFT + A",
   consuming = "game-only"
 }
@@ -13,7 +13,7 @@ local become_an_hero =
 {
   type = "custom-input",
   name = hotkeys.suicide,
-  localised_names = hotkeys.suicide,
+  localised_name = {hotkeys.suicide},
   key_sequence = "DELETE",
   consuming = "game-only"
 }
