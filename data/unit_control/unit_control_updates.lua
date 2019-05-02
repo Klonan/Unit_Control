d633ce53da52185b55ceff731f7200ed4be22860
Klonan <11986037+Klonan@users.noreply.github.com>
2019-05-02 14:02:31 +0200

    bumparoo + make kyranzor happy

--- data/unit_control/unit_control_updates.lua
+++ data/unit_control/unit_control_updates.lua
@@ -6,16 +6,14 @@ local deployer_tool = data.raw["selection-tool"][tools.deployer_selection_tool]
 local entity_filter = deployer_tool.entity_filters
 local alt_filter = deployer_tool.alt_entity_filters
 
-for name, entity in pairs (data.raw["assembling-machine"]) do
-  if entity.is_deployer then
-    table.insert(entity_filter, entity.name)
-    table.insert(alt_filter, entity.name)
-  end
-end
-
-for name, entity in pairs (data.raw["furnace"]) do
-  if entity.is_deployer then
-    table.insert(entity_filter, entity.name)
-    table.insert(alt_filter, entity.name)
+for entity_type, bool in pairs (util.entity_types()) do
+  local entities = data.raw[entity_type]
+  if entities then
+    for name, entity in pairs (entities) do
+      if entity.is_deployer then
+        table.insert(entity_filter, entity.name)
+        table.insert(alt_filter, entity.name)
+      end      
+    end
   end
 end
